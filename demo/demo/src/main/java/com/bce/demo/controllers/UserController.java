b50bd14dc0ee28cc01a15fbec97e3a4f4a482ed2
CentauroKing <valencia.p.marco.v@gmail.com>
2025-07-08 18:15:13 -0500

    En clase con deleteUser

--- demo/demo/src/main/java/com/bce/demo/controllers/UserController.java
+++ demo/demo/src/main/java/com/bce/demo/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.bce.demo.controllers;
 
+import java.nio.file.Path;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,17 +21,32 @@ import com.bce.demo.controllers.services.UserService;
 @CrossOrigin("*") // Allow all origins for simplicity; adjust as needed
 
 public class UserController {
-    
+
     @Autowired
     private UserService userService;
 
     @GetMapping("")
-    public List<User> getAllUser() {
+    public List<User> getAllUser(int UserId) {
         return userService.findAll();        
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable("userId") Integer userid) {
+        return userService.getUserById(userid);        
     }
 
     @PostMapping("/add")
     public User createUser(@RequestBody User user) {
         return userService.create(user);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateUser(@PathVariable("userId") Integer userId, @RequestBody User user) {
+        return userService.updateUser(userId, user);
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable("userId") Integer userId) {
+        userService.deleteUser(userId);
     }
 }
--- demo/demo/src/main/java/com/bce/demo/controllers/services/UserService.java
+++ demo/demo/src/main/java/com/bce/demo/controllers/services/UserService.java
@@ -8,15 +8,34 @@ import org.springframework.stereotype.Service;
 import com.bce.demo.controllers.User;
 import com.bce.demo.dao.UserRepository;
 
+
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
     public List<User> findAll() {
         return userRepository.findAll();
     }
     public User create(User user) {
         return userRepository.save(user);
+    }
+    public User getUserById(Integer userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + userId));
+    }
+    public User updateUser(Integer userId, User user) {
+        if(userRepository.existsById(userId)) {
+            user.setId(userId);
+            return userRepository.save(user);
+        }
+        return null;
+    }
+    
+    public void deleteUser(Integer userId) {
+        if (userRepository.existsById(userId)) {
+            userRepository.deleteById(userId);
+        }
     }
     // This service class can be used
 }
